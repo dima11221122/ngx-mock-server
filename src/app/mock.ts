6de4a8c91e2f36b0ee8c6ff70a48753f2ede52a9
Dmitry Snytkin <dimansny@bk.ru>
2018-06-17 14:23:22 +0400

    Fixed build

--- src/app/mock.ts
+++ src/app/mock.ts
@@ -1,18 +1,17 @@
 import { json, RouteDeclaration } from '../../mock-server/module/index';
 import { HttpRequest, HttpResponse } from '@angular/common/http';
-import { MockRequestParameters } from '../../mock-server/module/interfaces/mock-request-parameters';
 
 const users = [
   { id: 1, username: 'user1' },
   { id: 2, username: 'user2' }
 ];
 
-export const usersCallback = (req?: HttpRequest<any>): Promise<HttpResponse<any>> => {
+export function usersCallback(req?: HttpRequest<any>): Promise<HttpResponse<any>> {
   const query = req.params.get('query');
   return json(200, query ? users.filter(it => it.username.match(query)) : users);
 }
 
-export const userCallback = (req?: HttpRequest<any>): Promise<HttpResponse<any>> => {
+export function userCallback(req?: HttpRequest<any>): Promise<HttpResponse<any>> {
   const id = +req.url.match(/\d+/)[0];
   return json(200, users.find(it => it.id === id));
 }
--- src/app/modules/lazy/mock.ts
+++ src/app/modules/lazy/mock.ts
@@ -1,6 +1,6 @@
 import { json, RouteDeclaration } from '../../../../mock-server/module';
 
-export const itemsCallback = () => {
+export function itemsCallback() {
   return json(200, [
     {id: 1, name: 'item1'},
     {id: 2, name: 'item2'}
